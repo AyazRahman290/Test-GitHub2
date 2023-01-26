4021e5dbd590a5fa9023736b14866d6d91e04de5
AyazRahman290 <114905654+AyazRahman290@users.noreply.github.com>
2023-01-26 15:30:55 +0800

    Codes Updated

---
+++ constants.dart
@@ -0,0 +1,11 @@
+const String splashScreen1 = '/splashScreen';
+const String homeRoute = '/';
+const String premiumLoginRoute = '/premiumLogin';
+const String premiumRegistrationRoute = '/premiumRegistration';
+const String guestDashRoute = '/guestDash';
+const String registeredDashRoute = '/registeredDash';
+const String addCategoryRoute = '/addCatergory';
+const String addExpenseRoute = '/addExpense';
+const String viewExpenseRoute = '/viewExpense';
+const String viewBudgetLimitRoute = '/viewBudgetLimit';
+const String updateBudgetLimitRoute = '/updateBudgetLimit';
